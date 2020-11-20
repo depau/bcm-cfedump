32dd505e210e366bbd248023ad8945e77c0efa8c
hpo14 <5903619+hpo14@users.noreply.github.com>
2020-11-21 01:35:30 +0800

    ignore some read errors

--- bcm_cfedump.py
+++ bcm_cfedump.py
@@ -245,6 +245,22 @@ class CFEParserBase:
             if line.startswith(b"-----"):
                 break
 
+            # ignore Uncorrectable errors
+            if line.startswith(b"Uncorrectable ECC Error"):
+                continue
+
+            # ignore more errors
+            if line.startswith(b"nand_flash_read_buf(): Att"):
+                continue
+
+            if line.startswith(b"Error reading block"):
+                continue
+
+            #danitool begin: eat crashing line
+            if line.startswith(b"Correctable ECC Error detected"):
+                continue
+            #danitool end
+
             if len(line) == 0:
                 continue
 
